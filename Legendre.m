48adc99afac1eb302b0d2a5ae1cf9853ce5f7e34
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-05 14:28:30 +0300

    Update Legendre.m

--- Legendre.m
+++ Legendre.m
@@ -6,10 +6,38 @@ clear all; close all; clc;
 % yöntem sonuçları ile karşılaştırarak irdeleyiniz. Kullandığınız yöntemi, 
 % şekil ve grafiklerle anlatan bir rapor düzenleyiniz.
 
-n = 0;
+n = 5;
 m = 0;
-d = 2;
-dt = 2;
-t = 2;
-P_nm = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
-fprintf('P_nm: %d\n', P_nm);
+d = 5;
+dt = 5;
+t = 5;
+%P_n_m = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
+%fprintf('P_n_m: %d\n', P_n_m);
+for n = 1:0:n
+    if (n >= 2 && m == 0)
+        for n = 0:1:n
+            if (n >= 0 && n - 1 <= 2)
+                n = n - 1;
+                P_n_1_t = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
+                %return;
+            end
+            if (n >= 0 && n - 2 <= 2)
+                n = n -2;
+                P_n_2_t = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
+                %return;
+            end
+            P_n_t = ((2 * n - 1) / n) * t * P_n_1_t - ((n - 1) / n) * P_n_2_t;
+            %return;
+            fprintf('P_n_t: %d\n', P_n_t);        
+        end
+    elseif (n < 2 && m == 0)
+        for n = 0:1:n
+            if (n < 2)
+                P_n_m = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
+                fprintf('P_n_m: %d\n', P_n_m);
+            end 
+        end
+    end
+end
+
+
