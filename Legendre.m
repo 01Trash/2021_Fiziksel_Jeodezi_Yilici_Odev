b9816625831d4385c1e0f24371c0f718ae986827
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-09 00:26:22 +0300

    Update Legendre.m

--- Legendre.m
+++ Legendre.m
@@ -1,45 +1,35 @@
 clear all; close all; clc;
 
-%  Verilen keyfi bir enlem değerine göre tam normalleştirilmiş bütünleşik 
-% Legendre fonksiyon değerlerini yineleme bağntıları yardımıyla hesaplayan 
-% bir kod geliştiriniz. Fonksiyon değerlerini tam normalleştirilmemiş 
-% yöntem sonuçları ile karşılaştırarak irdeleyiniz. Kullandığınız yöntemi, 
-% şekil ve grafiklerle anlatan bir rapor düzenleyiniz.
+syms t x
 
-n = 5;
-m = 0;
-d = 5;
-dt = 5;
+n = 1;
+m = 3;
+%Q = (0:0.01:pi/2);
+Q = 1;
 t = 1;  %t = cos(Q)
-%P_n_m = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
-%fprintf('P_n_m: %d\n', P_n_m);
-for n = 1:n
-    if (n >= 2 && m == 0)
-        for n = 1:n
-            if (n >= 0 && n - 1 <= 2)
-                n = n + 1;
-                P_n_1_t = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
-                %return;
-                fprintf('P_n_1_t: %d\n', P_n_1_t); 
-            end
-            if (n >= 0 && n - 2 <= 2)
-                n = n + 2;
-                P_n_2_t = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
-                %return;
-                fprintf('P_n_2_t: %d\n', P_n_2_t); 
-            end
-            P_n_t = ((2 * n - 1) / n) * t * P_n_1_t - ((n - 1) / n) * P_n_2_t;
-            %return;
-            fprintf('P_n_t: %d\n', P_n_t);        
-        end
-    elseif (n < 2 && m == 0)
-        for n = 0:1:n
-            if (n < 2)
-                P_n_m = ((-1) ^ m) * (1 / ((2 ^ n) * factorial(n))) * ((1 - (t ^ 2)) ^ (m / 2)) * ((d ^ (n + m) / (dt ^ (n + m)))) * (((t ^ 2) - 1) ^ n);
-                fprintf('P_n_m: %d\n', P_n_m);
-            end 
-        end
-    end
-end
+x = n + m;
+
+%%% 47
+%Pnm => P_n_m
+P_n_m = ((-1).^m) * (1 / (2.^n * factorial(n))) * ((1 - t.^2).^(m / 2)) * diff((t.^2 - 1).^n, t, x);
+fprintf('P_n_m: %d\n', P_n_m); 
+
+%%% 48
+%Pn(t) => P_n_t
+%Pn-1(t) => P_n_1_t
+%Pn-2(t) => P_n_2_t
+P_n_1_t = 1;
+P_n_2_t = 1;
+P_n_t = ((2 * n - 1) / n) * t * P_n_1_t - ((n - 1) / n) * P_n_2_t;
+fprintf('P_n_t: %d\n', P_n_t); 
+
+%%% 48
+%Pnm(t) => P_n_m_t
+%Pn-2,m(t) => P_n_2_m_t
+%Pn-1,m-1(t) => P_n_1_m_1_t
+P_n_2_m_t = 1;
+P_n_1_m_1_t = 1;
+P_n_m_t = P_n_2_m_t + (2 * n - 1) * sqrt(1 - t.^2) * P_n_1_m_1_t;
+fprintf('P_n_m_t: %d\n', P_n_m_t); 
 
 
