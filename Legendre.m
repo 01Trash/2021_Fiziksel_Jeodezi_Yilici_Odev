5d05750800332558ab9c95c9711d22f4a07d1251
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-09 01:12:10 +0300

    Update Legendre.m

--- Legendre.m
+++ Legendre.m
@@ -45,6 +45,12 @@ for n = 1:n
             ((1 - t.^2).^(m / 2)) * diff((t.^2 - 1).^n, t, x);
         fprintf('P_n_m: %d\n', P_n_m);
     elseif n >= 2 && m == 0 %Pn(t) => P_n_t değeri
+        n = n - 1;
+        if n < 2 %Pnm => P_n_m değeri
+            P_n_m = ((-1).^m) * (1 / (2.^n * factorial(n))) * ...
+                ((1 - t.^2).^(m / 2)) * diff((t.^2 - 1).^n, t, x);
+            fprintf('P_n_m: %d\n', P_n_m);
+        end
         P_n_t = ((2 * n - 1) / n) * t * P_n_1_t - ((n - 1) / n) * P_n_2_t;
         fprintf('P_n_t: %d\n', P_n_t);
     elseif n >= 2 && m >= 1 %Pnm(t) => P_n_m_t değeri
@@ -52,26 +58,6 @@ for n = 1:n
         fprintf('P_n_m_t: %d\n', P_n_m_t);
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
