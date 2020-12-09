b9bc4cbc6757939fed3146e46a7a233dbd311020
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-09 18:24:53 +0300

    Update and rename 1_Legendre.m to 1_Legendre_Fonksiyon.m

--- 1_Legendre_Fonksiyon.m
+++ 1_Legendre_Fonksiyon.m
@@ -21,8 +21,8 @@ clear; close all; clc;
 
 syms t
 
-n = 5;
-m = 5;
+n = 2;
+m = 2;
 %t = cos(Q)
 %Q = (0:0.01:pi/2);
 x = n + m;
@@ -30,21 +30,22 @@ x = n + m;
 %Legendre Fonksiyonları
 for m = 0:1:m
     %Pnm => P_n_m değeri (47)
-    if n < 2 
+    if n < 2
         P_n_m = ((-1).^m) * (1 / (2.^n * factorial(n))) * ...
-        ((1 - t.^2).^(m / 2)) * diff((t.^2 - 1).^n, t, x);
+            ((1 - t.^2).^(m / 2)) * diff((t.^2 - 1).^n, t, x);
         fprintf('P_n_m: %s\n', P_n_m);
         
-        %Denklemi numerikleştirme
-        P_n_m_fun = matlabFunction(P_n_m);
-        t_val = cos(0:pi/100:pi);
-        P_n_m_fun(t_val);
-        %Şekli çizdirme
-        figure
-        plot(P_n_m_fun(t_val))
+        %         %Denklemi numerikleştirme
+        %         P_n_m_fun = matlabFunction(P_n_m);
+        %         t_val = cos(0:pi/100:pi);
+        %         P_n_m_fun(t_val);
+        %         %Şekli çizdirme 2d
+        %         figure
+        %         plot(P_n_m_fun(t_val))
+        %         %Şekil çizdirme 3d
         
-    %Pn(t) => P_n_t değeri (48) ∀ n ≥ 2, m = 0
-    elseif n >= 2 && m == 0 
+        %Pn(t) => P_n_t değeri (48) ∀ n ≥ 2, m = 0
+    elseif n >= 2 && m == 0
         %Pn-1(t) => P_n_1_t hesabı
         a = n - 1;
         %fprintf('a: %d\n', a);
@@ -72,12 +73,13 @@ for m = 0:1:m
         P_n_t_fun = matlabFunction(P_n_t);
         t_val = cos(0:pi/100:pi);
         P_n_t_fun(t_val);
-        %Şekli çizdirme
-        figure
+        %Şekli çizdirme 2d
+        hold on
         plot(P_n_t_fun(t_val))
+        %Şekil çizdirme 3d
         
-    %Pnm(t) => P_n_m_t değeri (49) ∀ n ≥ 2, m ≥ 1
-    elseif n >= 2 && m >= 1 
+        %Pnm(t) => P_n_m_t değeri (49) ∀ n ≥ 2, m ≥ 1
+    elseif n >= 2 && m >= 1
         %Pn-2,m(t) => P_n_2_m_t hesabı
         c = n - 2;
         %fprintf('c: %d\n', c);
@@ -103,23 +105,10 @@ for m = 0:1:m
         P_n_m_t_fun = matlabFunction(P_n_m_t);
         t_val = cos(0:pi/100:pi);
         P_n_m_t_fun(t_val);
-        %Şekli çizdirme
-        figure
+        %Şekli çizdirme 2d
+        hold on
         plot(P_n_m_t_fun(t_val))
+        %Şekil çizdirme 3d
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
